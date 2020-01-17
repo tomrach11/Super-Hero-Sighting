abd9f03e040d9df9d4b2bd61507e81800a6e3e1e
tomrach11 <tomrach11@gmail.com>
2020-01-16 21:50:30 -0500

    method on rock paper scissors

--- InteliJ/src/com/tsg/m1/assessment/RockPaperScissors.java
+++ InteliJ/src/com/tsg/m1/assessment/RockPaperScissors.java
@@ -16,48 +16,31 @@ public class RockPaperScissors {
             int botWin = 0;
             int totalTie = 0;
             int round = 0;
+            String roundResult;
             System.out.println("How many round do you want to play? [1-10]");
             round = Integer.parseInt(sc.nextLine());
             //verify round
             if (round >= 1 && round <= 10) {
                 //game start
                 for (int i = 0; i < round; i++) {
-                    //input hands
-                    System.out.println("-----------------------------------------");
-                    System.out.println("Please Choose you hand:");
-                    System.out.println("Type: 1=>Rock, 2=>Scissors, 3=>Paper");
-                    playerHand = Integer.parseInt(sc.nextLine());
+                    playerHand = inputPlayerHand();
                     botHand = random.nextInt(3 - 1) + 1;
-                    //show result of round
+                    //print player and bot hands
                     System.out.println(">> Your Hand: " + hands[playerHand - 1] + ", Bot Hand: " + hands[botHand - 1]);
-                    if (playerHand == 1 && botHand == 2) {
+
+                    roundResult = checkHands(playerHand, botHand);
+                    if ("playerWin".equals(roundResult)) {
                         playerWin++;
                         System.out.println("You WIN this round!!");
-                    } else if (playerHand == 2 && botHand == 3) {
-                        playerWin++;
-                        System.out.println("You WIN this round!!");
-                    } else if (playerHand == 3 && botHand == 1) {
-                        playerWin++;
-                        System.out.println("You WIN this round!!");
-                    } else if (playerHand == botHand) {
+                    } else if ("tie".equals(roundResult)) {
                         totalTie++;
                         System.out.println("We TIE this round!!");
                     } else {
                         botWin++;
-                        System.out.println("Sorry you LOST this round.");
+                        System.out.println("You LOST this round!!");
                     }
                 }
-                //shows winner
-                System.out.println("-----------------------------------------");
-                System.out.println("The score: Player Win-" + playerWin + " , Bot Win-" + botWin);
-                if (playerWin > botWin) {
-                    System.out.println("Congratulations you WIN the game!!");
-                } else if (botWin > playerWin) {
-                    System.out.println("Sorry you LOST the game");
-                } else if (playerWin == botWin) {
-                    System.out.println("We are TIE this game");
-                }
-                System.out.println("-----------------------------------------");
+                showFinalResult(playerWin, botWin);
                 //play again
                 System.out.println("Do you want to play again? [Y][N]");
                 if (!("Y".equals(sc.nextLine()))) {
@@ -71,5 +54,42 @@ public class RockPaperScissors {
                 System.out.println("Error: program is shutting down.");
             }
         } while (keepGoing == true);
+    }
+    public static String checkHands (int playerHand, int botHand) {
+        if (playerHand == 1 && botHand == 2 || playerHand == 2 && botHand == 3 || playerHand == 3 && botHand == 1) {
+            return "playerWin";
+        } else if (playerHand == botHand) {
+            return "tie";
+        } else {
+            return "botWin";
+        }
+    }
+    public static void showRoundResult (String result) {
+        if ("playerWin".equals(result)) {
+            System.out.println("You WIN this round!!");
+        } else if ("tie".equals(result)) {
+            System.out.println("We TIE this round!!");
+        } else {
+            System.out.println("You LOST this round!!");
+        }
+    }
+    public static void showFinalResult (int playerWin, int botWin) {
+        System.out.println("-----------------------------------------");
+        System.out.println("The score: Player Win-" + playerWin + " , Bot Win-" + botWin);
+        if (playerWin > botWin) {
+            System.out.println("Congratulations you WIN the game!!");
+        } else if (botWin > playerWin) {
+            System.out.println("Sorry you LOST the game");
+        } else if (playerWin == botWin) {
+            System.out.println("We are TIE this game");
+        }
+        System.out.println("-----------------------------------------");
+    }
+    public static int inputPlayerHand () {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("-----------------------------------------");
+        System.out.println("Please Choose you hand:");
+        System.out.println("Type: 1=>Rock, 2=>Scissors, 3=>Paper");
+        return Integer.parseInt(sc.nextLine());
     }
 }
